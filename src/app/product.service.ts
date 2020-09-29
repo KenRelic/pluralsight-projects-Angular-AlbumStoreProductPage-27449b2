eb36fde0c23ad6b14ab26bfed788c536f9ea8856
kenrelic <simeonkenneth3@gmail.com>
2020-09-29 21:43:54 +0100

    module4: adding the tracklisting component

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,14 +1,17 @@
 import 'rxjs/add/operator/map';
 import { Injectable } from '@angular/core';
 import { Http, Response } from '@angular/http';
+import {Observable} from 'rxjs/Observable';
+import {Album} from './album';
 
 @Injectable()
 export class ProductService {
 
   constructor(private _http: Http) {}
   private _albumUrl = '../assets/album.json';
-  getAlbum(id: number) {
-   return this._http.get(this._albumUrl).map((response: Response) => response.json);
+  getAlbum(id: number):Observable<Album> {
+   return this._http.get(this._albumUrl)
+   .map(response => <Album>response.json);
   }
 
 }
